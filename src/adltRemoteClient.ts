f8e56d5d961101236450597e89176c158ac03b49
Matthias Behr <mbehr@mcbehr.de>
2023-10-28 09:38:19 +0200

    feat: quick,dirty,hackish copy+paste from fishbone
    Needs proper refactoring!

---
+++ src/adltRemoteClient.ts
@@ -0,0 +1,421 @@
+import { WebSocket } from 'ws'
+import * as remote_types from './remote_types.js'
+
+export type MsgType = remote_types.BinType
+export type FileBasedMsgsHandler = (msg: remote_types.BinType) => void
+
+export interface NewMessageSink {
+  onNewMessages?: (nrNewMsgs: number) => void
+  onDone?: () => void
+  onStreamInfo?: (nrStreamMsgs: number, nrMsgsProcessed: number, nrMsgsTotal: number) => void
+}
+
+// map ecu/apid/ctids
+export interface EAC {
+  e: string
+  a: string
+  c: string
+}
+
+const mapEAC: Map<number, EAC> = new Map()
+const maprEAC: Map<string, Map<string, Map<string, number>>> = new Map()
+let maxEAC: number = 0
+
+export function getIdxFromEAC(eac: EAC): number {
+  let eMap = maprEAC.get(eac.e)
+  if (eMap === undefined) {
+    eMap = new Map<string, Map<string, number>>()
+    maprEAC.set(eac.e, eMap)
+  }
+  let aMap = eMap.get(eac.a)
+  if (aMap === undefined) {
+    aMap = new Map<string, number>()
+    eMap.set(eac.a, aMap)
+  }
+  let idx = aMap.get(eac.c)
+  if (idx !== undefined) {
+    return idx
+  } else {
+    idx = ++maxEAC
+    aMap.set(eac.c, idx)
+    mapEAC.set(idx, eac)
+    return idx
+  }
+}
+export function getEACFromIdx(idx: number): EAC | undefined {
+  const eac = mapEAC.get(idx)
+  return eac
+}
+
+export function char4U32LeToString(char4le: number): string {
+  let codes = [char4le & 0xff, 0xff & (char4le >> 8), 0xff & (char4le >> 16), 0xff & (char4le >> 24)]
+  while (codes.length > 0 && codes[codes.length - 1] === 0) {
+    codes.splice(-1)
+  }
+  return String.fromCharCode(...codes)
+}
+
+export interface RestObject {
+  id: string | number
+  type: string
+  attributes?: object
+  // relationsships
+  // links
+  meta?: object
+}
+
+export interface DltLifecycleInfoMinIF {
+  ecu: string
+  ecuLcNr?: number // the number shown in the tree view
+  persistentId: number
+  lifecycleStart: Date
+  isResume?: boolean // is this a resumed lifecycle?
+  lifecycleResume?: Date // resume time if any. use this instead of lifecycleStart then.
+  lifecycleEnd: Date
+  getTreeNodeLabel(): string
+  tooltip: string
+  swVersions: string[]
+  apidInfos?: Map<string, { apid: string; desc: string; ctids: Map<string, string> }>
+  nrMsgs: number
+  //logMessages?: DltMsg[],
+  //decorationType?: vscode.TextEditorDecorationType,
+  //node?: LifecycleNode,
+}
+
+export interface FilterableDltMsg {
+  timeStamp: number // timestamp_dms [deci=0.1 ms]
+  mstp: number
+  mtin: number
+  //readonly mcnt: number,
+  ecu: string
+  apid: string
+  ctid: string
+  verbose: boolean
+  payloadString: string
+  lifecycle?: DltLifecycleInfoMinIF
+
+  asRestObject(idHint: number): RestObject
+}
+
+export interface ViewableDltMsg extends FilterableDltMsg {
+  receptionTimeInMs: number
+  index: number
+  mcnt: number
+}
+
+export const MTIN_LOG_strs: string[] = ['', 'fatal', 'error', 'warn', 'info', 'debug', 'verbose', '', '', '', '', '', '', '', '', '']
+
+class AdltMsg implements ViewableDltMsg {
+  _eac: EAC
+  index: number
+  htyp: number
+  receptionTimeInMs: number
+  timeStamp: number
+  // lifecycle?: DltLifecycleInfoMinIF | undefined
+  lifecycle_id: number
+  mcnt: number
+  mstp: number
+  mtin: number
+  verbose: boolean
+  payloadString: string
+
+  constructor(binMsg: remote_types.BinDltMsg /*, lifecycle?: DltLifecycleInfoMinIF*/) {
+    // cached ECU, APID, CTID:
+    this._eac = getEACFromIdx(
+      getIdxFromEAC({ e: char4U32LeToString(binMsg.ecu), a: char4U32LeToString(binMsg.apid), c: char4U32LeToString(binMsg.ctid) }),
+    )!
+
+    this.index = binMsg.index
+    this.receptionTimeInMs = Number(binMsg.reception_time / 1000n)
+    this.timeStamp = binMsg.timestamp_dms
+    //this.lifecycle = lifecycle
+    this.lifecycle_id = binMsg.lifecycle_id
+    this.htyp = binMsg.htyp
+    this.mcnt = binMsg.mcnt
+    this.mstp = (binMsg.verb_mstp_mtin >> 1) & 0x7
+    this.mtin = (binMsg.verb_mstp_mtin >> 4) & 0xf
+    this.verbose = (binMsg.verb_mstp_mtin & 0x01) === 0x01
+    this.payloadString = binMsg.payload_as_text
+  }
+  get ecu(): string {
+    return this._eac.e
+  }
+  get apid(): string {
+    return this._eac.a
+  }
+  get ctid(): string {
+    return this._eac.c
+  }
+
+  asRestObject(idHint: number): RestObject {
+    return {
+      id: this.index,
+      type: 'msg',
+      attributes: {
+        timeStamp: this.timeStamp,
+        ecu: this.ecu,
+        mcnt: this.mcnt,
+        apid: this.apid,
+        ctid: this.ctid,
+        mtin: MTIN_LOG_strs[this.mtin],
+        payloadString: this.payloadString,
+        lifecycle: this.lifecycle_id, // this.lifecycle ? this.lifecycle.persistentId : undefined,
+      },
+    }
+  }
+}
+
+export interface StreamMsgData {
+  msgs: AdltMsg[]
+  sink: NewMessageSink
+}
+
+export class AdltRemoteClient {
+  private webSocket?: WebSocket
+  adltVersion?: string
+  webSocketIsConnected: boolean = false
+  webSocketErrors: string[] = []
+
+  private _reqCallbacks: ((resp: string) => void)[] = []
+  private streamMsgs = new Map<number, StreamMsgData | remote_types.BinType[]>()
+
+  constructor(private fileBasedMsgsHandler: FileBasedMsgsHandler) {}
+
+  close() {
+    if (this.webSocket) {
+      // todo if connected???
+      this.webSocket.close()
+    }
+  }
+
+  /**
+   * connect to (an existing) adlt process via websocket
+   *
+   * Does not start the adlt process!
+   *
+   * @param wssUrl url of the adlt remote websocket. e.g. ws://localhost:7777
+   * @returns the adlt version connected to
+   */
+  connectToWebSocket(wssUrl: string): Promise<string> {
+    const webSocket = new WebSocket(wssUrl, [], { perMessageDeflate: false, origin: 'adlt-logs', maxPayload: 1_000_000_000 })
+    this.webSocket = webSocket
+    webSocket.binaryType = 'arraybuffer' // todo ArrayBuffer needed for sink?
+    return new Promise((resolve, reject) => {
+      let promiseWaits = true
+
+      webSocket.on('message', (data: ArrayBuffer, isBinary) => {
+        try {
+          if (isBinary) {
+            this.onBinaryMessage(data)
+          } else {
+            const text = data.toString()
+            if (text.startsWith('info:')) {
+              // todo still used?
+              console.info(`AdltRemoteClient.on(message) info:`, text)
+            } else if (this._reqCallbacks.length > 0) {
+              // response to a request:
+              // console.info(`AdltRemoteClient.on(message) response for request:`, text)
+              let cb = this._reqCallbacks.shift()
+              if (cb) {
+                cb(text)
+              }
+            } else {
+              console.warn(`AdltRemoteClient.on(message) unexpected text=`, text)
+            }
+          }
+        } catch (e) {
+          console.error(`ws on message got error:${e}`)
+        }
+      })
+      webSocket.on('upgrade', (response) => {
+        // console.log(`AdltRemoteClient.on(upgrade) got response:`) //, response)
+        let ah = response.headers['adlt-version']
+        this.adltVersion = ah && !Array.isArray(ah) ? ah : ah && Array.isArray(ah) ? ah.join(',') : undefined
+        if (this.adltVersion) {
+          /*
+        if (!semver.satisfies(this.adltVersion, MIN_ADLT_VERSION_SEMVER_RANGE)) {
+          vscode.window.showErrorMessage(
+            `Your adlt version is not matching the required version!\nPlease correct!\nDetected version is '${this.adltVersion}' vs required '${MIN_ADLT_VERSION_SEMVER_RANGE}.'`,
+            { modal: true },
+          )
+        } else {
+          console.log(`adlt.AdltDocumentProvider got matching adlt version ${this.adltVersion} vs ${MIN_ADLT_VERSION_SEMVER_RANGE}.`)
+        }*/
+          //console.log(`AdltRemoteClient.on(upgrade) got adlt version:${this.adltVersion}`)
+        }
+      })
+      webSocket.on('open', () => {
+        this.webSocketIsConnected = true
+        this.webSocketErrors = []
+        if (promiseWaits) {
+          promiseWaits = false
+          resolve(this.adltVersion || 'no version')
+        }
+        //this.openAdltFiles()
+      })
+
+      webSocket.on('close', () => {
+        this.webSocketIsConnected = false
+        this.webSocketErrors.push('wss closed')
+        // console.warn(`AdltRemoteClient.on(close) wss got close`)
+        // this.emitStatusChanges.fire(this.uri)
+      })
+      webSocket.on('error', (err) => {
+        console.warn(`AdltRemoteClient.on(error) wss got error:`, err)
+        this.webSocketErrors.push(`error: ${err}`)
+        if (promiseWaits) {
+          promiseWaits = false
+          reject(err)
+        }
+        // this.emitStatusChanges.fire(this.uri)
+      })
+    })
+  }
+
+  sendAndRecvAdltMsg(req: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      if (this.webSocket) {
+        this._reqCallbacks.push((response: string) => {
+          // if we get an error/n ok we do reject as well:
+          if (response.startsWith('ok:')) {
+            resolve(response)
+          } else {
+            console.warn(`AdltRemoteClient.sendAndRecvAdltMsg got nok ('${response}') for request '${req}'`)
+            reject(response)
+          }
+        })
+        this.webSocket.send(req, (err) => {
+          if (err) {
+            console.warn(`AdltRemoteClient.sendAndRecvAdltMsg wss got error:`, err)
+            this.webSocketErrors.push(`wss send failed with:${err}`)
+            // todo! this.emitStatusChanges.fire(this.uri);
+          }
+        })
+      } else {
+        console.error(`AdltRemoteClient.sendAndRecvAdltMsg got no webSocket yet!`)
+        reject(`AdltRemoteClient.sendAndRecvAdltMsg got no webSocket yet!`)
+      }
+    })
+  }
+
+  private onBinaryMessage(data: ArrayBuffer) {
+    const bin_type = remote_types.readBinType(data)
+    switch (bin_type.tag) {
+      case 'DltMsgs':
+      case 'StreamInfo':
+        {
+          let streamId = bin_type.tag === 'DltMsgs' ? bin_type.value[0] : bin_type.value.stream_id
+          let streamData = this.streamMsgs.get(streamId)
+          if (streamData && !Array.isArray(streamData)) {
+            this.processBinStreamMsgs(bin_type, streamData)
+          } else {
+            // we store the pure data for later processing:
+            if (!streamData) {
+              streamData = [bin_type]
+              this.streamMsgs.set(streamId, streamData)
+            } else {
+              streamData.push(bin_type)
+              if (streamData.length > 3) {
+                console.warn(
+                  `adlt.on(binary): appended ${bin_type.tag} for yet unknown stream=${streamId}, streamData.length=${streamData.length}`,
+                )
+              }
+            }
+          }
+        }
+        break
+      case 'FileInfo':
+      case 'Lifecycles':
+      case 'EacInfo':
+      case 'PluginState':
+        {
+          this.fileBasedMsgsHandler(bin_type)
+        }
+        break
+      default:
+        console.warn(`ws on binary message ignored:${(bin_type as unknown as any).tag}`)
+        break
+    }
+  }
+
+  processBinStreamMsgs(bin_type: remote_types.BinType, streamData: StreamMsgData) {
+    switch (bin_type.tag) {
+      case 'DltMsgs':
+        const [streamId, msgs] = bin_type.value
+        if (msgs.length === 0) {
+          // indicates end of query:
+          if (streamData.sink.onDone) {
+            streamData.sink.onDone()
+          }
+          this.streamMsgs.delete(streamId)
+          //console.error(`adlt.processBinDltMsgs deleted stream #${streamId}\n\n`)
+        } else {
+          for (let i = 0; i < msgs.length; ++i) {
+            let binMsg = msgs[i]
+
+            let msg = new AdltMsg(binMsg /*, this.lifecycleInfoForPersistentId(binMsg.lifecycle_id)*/)
+            streamData.msgs.push(msg)
+          }
+          if (streamData.sink.onNewMessages) {
+            streamData.sink.onNewMessages(msgs.length)
+          }
+        }
+        break
+      case 'StreamInfo':
+        const si = bin_type.value
+        // console.log(`adlt.processBinStreamMsgs: StreamInfo stream=${si.stream_id}, stream msgs=${si.nr_stream_msgs} processed=${si.nr_file_msgs_processed} total=${si.nr_file_msgs_total}`);
+        if (streamData.sink.onStreamInfo) {
+          streamData.sink.onStreamInfo(si.nr_stream_msgs, si.nr_file_msgs_processed, si.nr_file_msgs_total)
+        }
+        break
+    }
+  }
+
+  /**
+   * calculate and return the matching messages. Does not modify the current content/view.
+   * @param filters list of filters to use. Should only be pos and neg filters. Others will be ignored.
+   * @param maxMsgsToReturn maximum number of messages to return. As this is no async function the caller
+   * needs to be careful!
+   * @returns list of matching messages (as Promise)
+   */
+  getMatchingMessages(filters: any[], maxMsgsToReturn: number): Promise<FilterableDltMsg[]> {
+    let p = new Promise<FilterableDltMsg[]>((resolve, reject) => {
+      const matchingMsgs: AdltMsg[] = []
+      // sort the filters here into the enabled pos and neg:
+      try {
+        let filterStr = filters
+          .filter((f) => ('enabled' in f ? f.enabled : true))
+          .map((f) => JSON.stringify({ ...f, enabled: true }))
+          .join(',')
+        this.sendAndRecvAdltMsg(`query {"window":[0,${maxMsgsToReturn}], "filters":[${filterStr}]}`)
+          .then((response) => {
+            const streamObj = JSON.parse(response.substring(10))
+
+            let sink: NewMessageSink = {
+              onDone() {
+                // console.log(`adlt.getMatchingMessages done matchingMsgs.length=${matchingMsgs.length}`)
+                resolve(matchingMsgs)
+              },
+            }
+            // here some data might be already there for that stream.
+            // this can happen even though the wss data arrives sequentially but the processing
+            // here for wss data is a direct call vs. an asyn .then()...
+            let curStreamMsgData = this.streamMsgs.get(streamObj.id)
+            let streamData = { msgs: matchingMsgs, sink: sink }
+            this.streamMsgs.set(streamObj.id, streamData)
+            if (curStreamMsgData && Array.isArray(curStreamMsgData)) {
+              // process the data now:
+              curStreamMsgData.forEach((msgs) => this.processBinStreamMsgs(msgs, streamData))
+            }
+          })
+          .catch((reason) => {
+            reject(reason)
+          })
+      } catch (e) {
+        throw new Error(`getMatchingMessages failed due to error '${e}'`)
+        reject(e)
+      }
+    })
+    return p
+  }
+}
--- src/cmdExec.ts
+++ src/cmdExec.ts
@@ -2,9 +2,15 @@
  * todos:
  * [] add glob support for files passed as arguments
  */
+import fs from 'fs'
+
+import { default as JSON5 } from 'json5'
+import { default as jp } from 'jsonpath'
 
 import chalk from 'chalk'
 import { MultiBar } from 'cli-progress'
+import { AdltRemoteClient, FileBasedMsgsHandler, MsgType, char4U32LeToString } from './adltRemoteClient.js'
+import { FBBadge, getFBDataFromText, iterateAllFBElements, rqUriDecode } from './fbaFormat.js'
 
 const error = chalk.bold.red
 const warning = chalk.bold.yellow
@@ -14,6 +20,9 @@ export const cmdExec = async (files: string[], options: any) => {
 
   const fbaFiles: string[] = []
   const nonFbaFiles: string[] = []
+
+  const pluginCfgs = JSON.stringify([]) // todo
+  const sortOrderByTime = 'sortOrderByTime' in options ? !!options.sortOrderByTime : true
 
   for (const file of files) {
     // check whether file exists otherwise apply glob pattern first todo
@@ -39,25 +48,205 @@ export const cmdExec = async (files: string[], options: any) => {
       hideCursor: true,
       format: ' {bar} | {percentage}% | {value}/{total} | {file}',
     })
-    const barAdlt = multibar.create(1, 0, { file: 'starting adlt' })
+    const barAdlt = multibar.create(5, 0, { file: 'starting adlt' })
     const barMsgsLoadedOptions = {
-      format: ' {bar} | {percentage}% | {duration}s/{eta}s | msgs processed',
+      format: '                                      {duration}s | {total} msgs read',
     }
     const barMsgsLoaded = multibar.create(100, 0, {}, barMsgsLoadedOptions)
     const barFbas = multibar.create(fbaFiles.length, 0)
+
+    const fileBasedMsgsHandler: FileBasedMsgsHandler = (msg: MsgType) => {
+      switch (msg.tag) {
+        case 'FileInfo':
+          const fi = msg.value
+          barMsgsLoaded.setTotal(fi.nr_msgs)
+          break
+        case 'Lifecycles':
+          const li = msg.value
+          multibar.log(`Got ${li.length} lifecycles ${char4U32LeToString(li[0]?.ecu || 0)}\n`)
+          break
+      }
+    }
+
+    const adltClient = new AdltRemoteClient(fileBasedMsgsHandler)
     // start adlt
     multibar.log(`Starting adlt...\n`)
-    // wait 5s
-    await new Promise((resolve) => setTimeout(resolve, 5000))
-    barAdlt.increment(1, { file: 'adlt started' })
-    // load dlt files
-    multibar.log(`Processing DLT files...\n`)
-    // exec the fba files
-    for (const fbaFile of fbaFiles) {
-      barFbas.increment(1, { file: fbaFile })
-      // console.log(`executing fba file:'${fbaFile}'`)
-      await new Promise((resolve) => setTimeout(resolve, 2000))
-    }
-    multibar.stop()
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    // todo add parameter!
+    //multibar.log(`Connecting to adlt...\n`)
+    barAdlt.increment(1, { file: 'adlt connecting' })
+    adltClient
+      .connectToWebSocket('ws://127.0.0.1:7777')
+      .then(async (adltVersion) => {
+        //multibar.log(`Connected to adlt...\n`)
+        barAdlt.increment(1, { file: 'adlt connected' })
+        // open files
+        await adltClient
+          .sendAndRecvAdltMsg(`open {"sort":${sortOrderByTime},"files":${JSON.stringify(nonFbaFiles)},"plugins":${pluginCfgs}}`)
+          .then(async (response) => {
+            // todo check response
+            // multibar.log(`Opened files...\n`)
+            barAdlt.increment(1, { file: 'adlt files opened' })
+            // console.log('response:', response)
+            await adltClient.getMatchingMessages([{ enabled: true, ecu: 'E002', type: 0 }], 1000).then(
+              (msgs) => {
+                multibar.log(`Got ${msgs.length} msgs\n`)
+              },
+              (e) => {
+                multibar.log(`Got ${e}  on getMatchingMsgs\n`)
+              },
+            )
+            // wait 5s
+            //await new Promise((resolve) => setTimeout(resolve, 5000))
+            barAdlt.increment(1, { file: 'adlt started' })
+            // load dlt files
+            //multibar.log(`Processing DLT files...\n`)
+            // exec the fba files
+            for (const fbaFile of fbaFiles) {
+              barFbas.increment(1, { file: fbaFile })
+              // console.log(`executing fba file:'${fbaFile}'`)
+              await processFbaFile(fbaFile, adltClient).then(
+                (nrOfFbas) => {
+                  //barFbas.increment(nrOfFbas)
+                },
+                (e) => {
+                  console.log(error(`Failed to process fba file:'${fbaFile}'! Got error:${e}`))
+                },
+              )
+            }
+          })
+          .catch((e) => {
+            console.log(error(`Failed to open DLT files! Got error:${e}`))
+          })
+      })
+      .catch((e) => {
+        console.log(error(`Failed to connect to adlt! Got error:${e}`))
+      })
+      .finally(() => {
+        adltClient.close()
+        multibar.stop()
+        console.log(warning('finally...'))
+      })
   }
+}
+
+const processBadge = async (badge: FBBadge, adltClient: AdltRemoteClient) => {
+  try {
+    console.log(` badge=${JSON.stringify(badge)}`)
+    if (badge.conv && badge.source.startsWith('ext:mbehr1.dlt-logs/')) {
+      const rq = rqUriDecode(badge.source)
+      // console.log(`rqCmd.path=${rqCmd.path}`)
+      if (rq.path.endsWith('/filters?')) {
+        console.log(`rq.commands=${JSON.stringify(rq.commands)}`)
+        for (const cmd of rq.commands) {
+          if (cmd.cmd === 'query') {
+            const conv = badge.conv
+            await adltClient.getMatchingMessages(JSON5.parse(cmd.param), 1000).then((msgs) => {
+              try {
+                // jsonPath expects restQuery results as:
+                // {data: [{id, type, attributes: {timeStamp, ecu, mcnt, ctid, apid, mtin, payloadString, lifecycle}}]}
+                const rqResult = {
+                  data: msgs.map((msg, idx) => msg.asRestObject(idx)),
+                }
+                // console.log(`got ${msgs.length} msgs. jsonPath=${badge.jsonPath}\n`)
+                const jsonPath = badge.jsonPath
+                let jsonPathResult: any[] | undefined
+                if (jsonPath) {
+                  jsonPathResult = jp.query(rqResult, jsonPath)
+                  //console.log(`jsonPathResult.length=${jsonPathResult.length}`)
+                }
+                const result = jsonPathResult !== undefined ? jsonPathResult : rqResult
+                const indexFirstC = conv.indexOf(':')
+                const convType = conv.slice(0, indexFirstC)
+                const convParam = conv.slice(indexFirstC + 1)
+                let convResult: string | number | undefined
+                switch (convType) {
+                  case 'length':
+                    convResult = Array.isArray(result) ? result.length : Array.isArray(result.data) ? result.data.length : 0
+                    break
+                  case 'index':
+                    convResult =
+                      Array.isArray(result) && result.length > Number(convParam)
+                        ? typeof result[Number(convParam)] === 'string'
+                          ? result[Number(convParam)]
+                          : JSON.stringify(result[Number(convParam)])
+                        : 0
+                    break
+                  case 'func':
+                    try {
+                      if (!(globalThis as any).JSON5) {
+                        ;(globalThis as any).JSON5 = JSON5
+                      }
+                      const fn = Function('result', convParam)
+                      const fnRes = fn(result)
+                      switch (typeof fnRes) {
+                        case 'string':
+                        case 'number':
+                          convResult = fnRes
+                          break
+                        case 'object':
+                          convResult = JSON.stringify(fnRes)
+                          break
+                        case 'undefined':
+                          break
+                        default:
+                          convResult = `unknown result type '${typeof fnRes}'. Please return string or number`
+                          break
+                      }
+                    } catch (e) {
+                      console.warn(`processBadg.msgs func got error:${e}`)
+                    }
+                    break
+                  default:
+                    break
+                }
+                if (convResult !== undefined) {
+                  console.log(`badge.conv='${convResult}'`)
+                }
+              } catch (e) {
+                console.warn(`processBadg.msgs got error:${e}`)
+              }
+            })
+          } else {
+            console.log(`rq.cmd=${cmd} ignored!`)
+          }
+        }
+      }
+    }
+  } catch (e) {
+    console.warn(`processBadge got error:${e}`)
+  }
+}
+
+const processFbaFile = async (filePath: string, adltClient: AdltRemoteClient) => {
+  // try to open the file:
+  return new Promise<number>((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, fbaFileContent) => {
+      if (err) {
+        console.log(error(`Failed to read fba file:'${filePath}'! Got error:${err}`))
+        reject(err)
+        return
+      }
+      try {
+        const fba = getFBDataFromText(fbaFileContent)
+        console.log(`fba.title=${fba.title}`)
+        iterateAllFBElements(fba.fishbone, [], async (fbType, fbElement, parent) => {
+          if (fbType === 'rc' && typeof fbElement === 'object' && fbElement.type === 'react' && fbElement.element === 'FBACheckbox') {
+            //console.log(`fbType=${fbType}, fbElement.type=${fbElement.type},fbElement.element=${fbElement.element} parent=${parent.name}`)
+            if (fbElement.props?.badge) {
+              processBadge(fbElement.props.badge, adltClient)
+            }
+            if (fbElement.props?.badge2) {
+              processBadge(fbElement.props.badge2, adltClient)
+            }
+          }
+        })
+        resolve(1)
+      } catch (e) {
+        reject(e)
+      }
+    })
+  })
+  // await new Promise((resolve) => setTimeout(resolve, 2000))
 }
